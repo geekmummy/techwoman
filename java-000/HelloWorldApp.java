9d2f38edcc10d07b231edffb174c1de913187449
Qiguer Zhang <349155823@qq.com>
2016-05-13 23:38:02 +0800

    this is my first java app

---
+++ java-000/HelloWorldApp.java
@@ -0,0 +1,10 @@
+class HelloWorldAp{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!");
+		System.out.println("I have a dream!");
+		System.out.println("it is to be a superwoman!");
+	}
+
+}
