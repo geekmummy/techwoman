11ddb245ab5da61fe41fe08db90dc664bfb4a4eb
Qiguer Zhang <349155823@qq.com>
2016-05-16 20:38:09 +0800

    add test domo for git

--- java-000/HelloWorldApp.java
+++ java-000/HelloWorldApp.java
@@ -5,6 +5,8 @@ class HelloWorldAp{
 		System.out.println("Hello World!");
 		System.out.println("I have a dream!");
 		System.out.println("it is to be a superwoman!");
+                // this is a modify test for git
+                system.out.println("hello git, hello github");
 	}
 
 }
